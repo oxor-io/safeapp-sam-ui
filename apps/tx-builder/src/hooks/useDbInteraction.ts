f15b92064aa0c3062c726e761782eb39fd4961af
dm.igorevich <dmigorevich@icloud.com>
2024-04-25 17:34:16 +0700

    feat(fetch by params): added fetch by params

--- apps/tx-builder/src/hooks/useDbInteraction.ts
+++ apps/tx-builder/src/hooks/useDbInteraction.ts
@@ -5,6 +5,7 @@ import { ProposedTransaction, SamTransaction } from '../typings/models'
 const REACT_APP_SUPABASE_URL = "https://snsoupmxxcbdyohaeeny.supabase.co"
 const REACT_APP_SUPABASE_KEY= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNuc291cG14eGNiZHlvaGFlZW55Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM4OTM2MzEsImV4cCI6MjAyOTQ2OTYzMX0.v8BGP1LFm1siAYXC7QYobH9bJ0y-tnzVMCqJkhOF4Eg"
 
+type TransactionParams = 'id' | 'contractInterface' | 'description' | 'raw' | 'nonce' | 'proofs' | 'confirmed' | 'address' | 'root'
 export const useDbInteraction = () => {
   const [isLoading, setIsLoading] = useState(false)
   const [transactions, setTransactions] = useState<SamTransaction[]>([])
@@ -69,9 +70,22 @@ export const useDbInteraction = () => {
     })
   }
 
+
+  const fetchTransactionByParam = async (param: TransactionParams, value: string) => {
+    return await fetch(`${REACT_APP_SUPABASE_URL}/rest/v1/transactions?${param}=eq.${value}&select=*`, {
+      method: 'GET',
+      headers: {
+        "Content-Type": 'application/json',
+        "apiKey": REACT_APP_SUPABASE_KEY ?? '',
+        Authorization: `Bearer ${REACT_APP_SUPABASE_KEY ?? ''}`,
+      },
+    })
+  }
+
   return {
     transactions,
     isLoading,
+    get: {all: fetchTransactions, byParam: fetchTransactionByParam},
     saveTransaction,
     updateTransaction,
     removeTransaction
